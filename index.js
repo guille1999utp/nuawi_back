f77e8c15353d99142e44abb71269c7b593d5aad7
guillermo penaranda hernandez <guillermo.penaranda@utp.edu.co>
2023-12-05 02:43:11 -0500

    probando

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 const express = require('express');
 const cors = require('cors');
-const app = express();
 require('./src/database');
+const app = express();
+const { swaggerDocs } = require('./swagger/swagger');
 
 const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
-const { swaggerDocs } = require('./swagger/swagger');
 //rutas
 app.use(require('./routes/index'));
 
 
+swaggerDocs(app,PORT);
 
 app.listen(PORT,()=>{
-    swaggerDocs(app,PORT);
 });
